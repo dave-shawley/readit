a7796dd2d0ea2891cf81f4ee8ce46c768e835be4
Dave Shawley <daveshawley@gmail.com>
2011-12-29 09:40:59 -0500

    Changed top-level interface to use the Flask app directly.

--- readit/__init__.py
+++ readit/__init__.py
@@ -1,22 +1,25 @@
+import os
+import StringIO
 import flask
 
 
 application = flask.Flask(__name__)
-DEFAULT_CONFIG = {
-        'host': '0.0.0.0',
-        'port': 5000,
-        'debug': False,
-        }
+application.HOST = '0.0.0.0'
+application.PORT = 5000
+
 
 @application.route('/')
 def fetch_read_list():
-    return "You ain't read nothin!"
+    response = StringIO.StringIO()
+    response.write('<html><head><title>Settings</title></head><body>')
+    response.write('<table>')
+    for (k, v) in os.environ.iteritems():
+        response.write('<tr><th>')
+        response.write(k)
+        response.write('</th><td>')
+        response.write(v)
+        response.write('</td></tr>')
+    response.write('</table></body></html>')
+    return response.getvalue()
 
-
-def get_defaults():
-    return DEFAULT_CONFIG.copy()
-
-def run(config):
-    application.run(host=config['host'], port=config['port'],
-            debug=config['debug'])
 
--- web.py
+++ web.py
@@ -2,9 +2,8 @@ import os
 import readit
 
 if __name__ == '__main__':
-    config = readit.get_defaults()
     port = os.environ.get('PORT')
     if port:
-        config['port'] = int(port)
-    readit.run(config)
+        readit.application.PORT = int(port)
+    readit.application.run()
 
