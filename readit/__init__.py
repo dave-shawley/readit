21d236a5bd8ed9ff7a646dd966dabc5bef767ae0
Dave Shawley <daveshawley@gmail.com>
2011-12-29 12:02:52 -0500

    Replaced inline HTML with Jinja templates and added MongoDB connections

--- readit/__init__.py
+++ readit/__init__.py
@@ -1,6 +1,8 @@
 import os
 import StringIO
+
 import flask
+import pymongo
 
 
 
@@ -8,34 +10,46 @@ application = flask.Flask(__name__)
 
 class Configuration(object):
     def __init__(self):
-        self.host = '0.0.0.0'
-        self.port = 5000
-        self.debug = False
-        self.mongo_url = 'mongodb://localhost/'
-        self.enable_config = False
+        self.host = os.environ.get('HOST', '0.0.0.0')
+        self.port = int(os.environ.get('PORT', '5000'))
+        value = os.environ.get('DEBUG', 'False').lower()
+        self.debug = value == 'true' or value == '1'
+        self.mongo_url = os.environ.get('MONGOURL', 'mongodb://localhost/')
+    def values(self):
+        return {'HOST': self.host, 'PORT': self.port, 'DEBUG': self.debug,
+                'MONGO_URL': self.mongo_url}
 
-class Response(StringIO.StringIO):
-    def add_table(self, name, data):
-        self.write('<table><caption><h2>' + name + '</h2></caption>')
-        for (k, v) in data.iteritems():
-            self.write('<tr><th>')
-            self.write(k)
-            self.write('</th><td>')
-            self.write(v)
-            self.write('</td></tr>')
-        self.write('</table>')
+
+@application.before_request
+def initialize_mongo_connection():
+    flask.g.db = None
+
+@application.after_request
+def teardown_mongo_connection(response):
+    if flask.g.db:
+        application.logger.debug('closing connection %s', str(flask.g.db))
+        flask.g.db.connection.close()
+    return response
+
+def connect_to_db():
+    if flask.g.db is None:
+        application.logger.debug('connecting to %s',
+                application.config['MONGO_URL'])
+        conn = pymongo.connection.Connection(
+                host=application.config['MONGO_URL'])
+        flask.g.db = conn.readit
 
 
 @application.route('/config')
 def fetch_read_list():
-    response = Response()
-    response.write('<html><head><title>Settings</title></head><body>')
-    response.add_table('application.config', application.config)
-    response.add_table('os.environ', os.environ)
-    response.write('</body></html>')
-    return response.getvalue()
+    if not application.config['DEBUG']:
+        flask.abort(403)
+    return flask.render_template('config.html', environ=os.environ)
 
 
-def run(config):
+def run(config=None):
+    if config is None:
+        config = Configuration()
+    application.config.update(config.values())
     application.run(host=config.host, port=config.port, debug=config.debug)
 
--- web.py
+++ web.py
@@ -2,15 +2,5 @@ import os
 import readit
 
 if __name__ == '__main__':
-    config = readit.Configuration()
-    value = os.environ.get('PORT')
-    if value:
-        config.port = int(value)
-    value = os.environ.get('MONGOURL')
-    if value:
-        config.mongo_url = value
-    value = os.environ.get('DEBUG')
-    if value:
-        config.debug = value.lower() == 'true' or str(value) == '1'
-    readit.run(config)
+    readit.run()
 
