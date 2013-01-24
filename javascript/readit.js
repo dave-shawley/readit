b60692808c34f552bb94f72580185ee350411c8d
Dave Shawley <daveshawley@gmail.com>
2013-01-23 22:04:28 -0500

    Reformatted Javascript code to make Crockford's conventions.

--- javascript/readit.js
+++ javascript/readit.js
@@ -1,84 +1,82 @@
-(function(jQuery, window, console) {
-	readit = function(spec) {
-		var	that = stdlib.object(),
-				args = spec || {},
-				DEFAULT_MESSAGE_SELECTOR = "#message",
+(function (jQuery, window, console) {
+    readit = function (spec) {
+        var that = stdlib.object();
+        var args = spec || {};
 
-				//
-				// CSS selectors
-				//
-				sel = {
-					reading_li: args.reading_li_selector || "#reading",
-					reading_info: args.reading_info_selector || "#readinginfo",
-					template: args.template_class || "template",
-					title: args.title_selector || ".title",
-					when: args.when_selector || ".when",
-					link: args.link_selector || ".link",
-					url: args.url_selector || ".url",
-					message: args.message_selectors || {}
-				},
+        //
+        // CSS selectors
+        //
+        var sel = {
+            reading_li: args.reading_li_selector || "#reading",
+            reading_info: args.reading_info_selector || "#readinginfo",
+            template: args.template_class || "template",
+            title: args.title_selector || ".title",
+            when: args.when_selector || ".when",
+            link: args.link_selector || ".link",
+            url: args.url_selector || ".url",
+            message: args.message_selectors || {}
+        };
 
-				//
-				// Instance variables
-				//
-				actions = args.actions || {},
-				debug = args.debug || false,
+        //
+        // Instance variables
+        //
+        var actions = args.actions || {};
+        var debug = args.debug || false;
 
-				//
-				// Optional *helper* hooks
-				//
-				setLocation = args.setLocation || function (url) {
-					window.location.href = url;
-				}
-			;
+        //
+        // Optional *helper* hooks
+        //
+        var setLocation = args.setLocation || function (url) {
+            window.location.href = url;
+        };
 
-		// Patch the message selector defaults
-		sel.message.selector = sel.message.selector || DEFAULT_MESSAGE_SELECTOR;
-		sel.message.status = sel.message.status || ".status";
-		sel.message.phrase = sel.message.phrase || ".phrase";
-		sel.message.error = sel.message.error || ".error";
+        // Patch the message selector defaults
+        sel.message.selector = sel.message.selector || "#message";
+        sel.message.status = sel.message.status || ".status";
+        sel.message.phrase = sel.message.phrase || ".phrase";
+        sel.message.error = sel.message.error || ".error";
 
-		//
-		// This is used as a constructor function to create new Reading objects
-		//
-		var Reading = function(data) {
-			data = data || {};
-			this.id = data.id;
-			this.title = data.title;
-			this.link = data.link;
-			this.when = new Date();
-			if (data.when) {
-				try {
-					var d = new Date(data.when); // this might throw an exception
-					d.toISOString(); // this should Error out if date is invalid
-					this.when = d; // yippeeee
-				} catch (e) {
-					// nothing to really do here... already assigned this.when
-				}
-			}
-			this.jsonify = function() {
-				return JSON.stringify({
-					title: this.title,
-					link: this.link,
-					when: this.when
-				});
-			};
-		};
+        //
+        // This is used as a constructor function to create new Reading objects
+        //
+        function Reading(data) {
+            data = data || {};
+            this.id = data.id;
+            this.title = data.title;
+            this.link = data.link;
+            this.when = new Date();
+            if (data.when) {
+                try {
+                    var d = new Date(data.when); // could throw an exception
+                    d.toISOString(); // should Error out if date is invalid
+                    this.when = d; // yippeeee! should have a valid data here
+                } catch (e) {
+                    // nothing to really do here... already assigned this.when
+                }
+            }
+            this.jsonify = function () {
+                return JSON.stringify({
+                    title: this.title,
+                    link: this.link,
+                    when: this.when
+                });
+            };
+        };
 
         //
         // Now for a few private/utility methods.
         //
-        var updateUiForReading = function($uiElement, aReading) {
+        function updateUiForReading($uiElement, aReading) {
             $uiElement.find(sel.title).text(aReading.title);
             $uiElement.find(sel.when).text(aReading.when.toDateString());
             $uiElement.find(sel.link).prop({
-            	"href": aReading.link,
-            	"rel": "external"
-			});
+                "href": aReading.link,
+                "rel": "external"
+            });
             $uiElement.find(sel.url).text(aReading.link);
         };
 
-        var debugMessage = function () {
+        function debugMessage() {
             if (!debug) {
                 return;
             }
@@ -90,7 +88,7 @@
             console.log("<< DEBUG");
         };
 
-        var wasRedirected = function (ajaxData) {
+        function wasRedirected(ajaxData) {
             if (ajaxData && ajaxData.hasOwnProperty("redirect_to")) {
                 debugMessage("handling redirect to " + ajaxData.redirect_to);
                 setLocation(ajaxData.redirect_to);
@@ -99,116 +97,116 @@
             return false;
         };
 
-		//
-		// Public Methods
-		//    These are exported by attaching them to ``that`` as we go.
-		//
-        var showReading = function(aReading) {
+        //
+        // Public Methods
+        //    These are exported by attaching them to ``that`` as we go.
+        //
+        function showReading(aReading) {
             updateUiForReading(jQuery(sel.reading_info), aReading);
         };
         that.addMethod("showReading", showReading,
-                       "Updates the UI to match a reading.");
-		var update = function() {
-			that.readings.forEach(function (aReading) {
-				var $elm = jQuery("#" + aReading.id);
-				if ($elm.length === 0) {
-					$elm = jQuery(sel.reading_li).clone();
-					$elm.prop("id", aReading.id);
-					$elm.removeClass(sel.template)
-					    .insertBefore(sel.reading_li);
-				}
-				$elm.click(function() { showReading(aReading); });
-				updateUiForReading($elm, aReading);
-			});
-		};
-		that.addMethod("update", update,
-			"Update the UI to match ``this.readings``.",
-			"No parameters, no answer.");
+            "Updates the UI to match a reading.");
+        function update() {
+            that.readings.forEach(function (aReading) {
+                var $elm = jQuery("#" + aReading.id);
+                if ($elm.length === 0) {
+                    $elm = jQuery(sel.reading_li).clone();
+                    $elm.prop("id", aReading.id);
+                    $elm.removeClass(sel.template)
+                        .insertBefore(sel.reading_li);
+                }
+                $elm.click(function () { showReading(aReading); });
+                updateUiForReading($elm, aReading);
+            });
+        };
+        that.addMethod("update", update,
+            "Update the UI to match ``this.readings``.",
+            "No parameters, no answer.");
 
-		var addReading = function(aReading) {
-			if (!actions['add-reading']) {
-				debugMessage('no add-reading action defined');
-				return;
-			}
-			jQuery.ajax({
-				url: actions['add-reading'].url,
-				type: actions['add-reading'].method,
-				data: JSON.stringify(aReading),
-				dataType: "json",
-				contentType: "application/json;charset=UTF-8",
-				success: function (data) {
-					debugMessage("addReading() success", data);
-					if (!wasRedirected(data)) {
-						that.readings.push(new Reading(data.new_reading));
-						that.update();
-						if (document.jqt) {
-							document.jqt.goBack();
-						}
-					}
-				}
-			});
-		};
-		that.addMethod("addReading", addReading,
-			"Adds a reading to the list of readings.",
-			"Requires a *reading* object that is sent to the server as-is.",
-			"Answers by pushing the response onto ``this.readings``.");
+        function addReading(aReading) {
+            if (!actions['add-reading']) {
+                debugMessage('no add-reading action defined');
+                return;
+            }
+            jQuery.ajax({
+                url: actions['add-reading'].url,
+                type: actions['add-reading'].method,
+                data: JSON.stringify(aReading),
+                dataType: "json",
+                contentType: "application/json;charset=UTF-8",
+                success: function (data) {
+                    debugMessage("addReading() success", data);
+                    if (!wasRedirected(data)) {
+                        that.readings.push(new Reading(data.new_reading));
+                        that.update();
+                        if (document.jqt) {
+                            document.jqt.goBack();
+                        }
+                    }
+                }
+            });
+        };
+        that.addMethod("addReading", addReading,
+            "Adds a reading to the list of readings.",
+            "Requires a *reading* object that is sent to the server as-is.",
+            "Answers by pushing the response onto ``this.readings``.");
 
-		var fetchReadings = function() {
-			if (!actions['get-readings']) {
-				debugMessage('no get-readings action defined');
-				return;
-			}
-			jQuery.ajax({
-				url: actions['get-readings'].url,
-				type: actions['get-readings'].method,
-				dataType: "json",
-				success: function (data) {
-					debugMessage("success", data);
-					that.readings = [];
-					data.readings.forEach(function (r) {
-						that.readings.push(new Reading(r));
-					});
-					that.update();
-				}
-			});
-		};
-		that.addMethod("fetchReadings", fetchReadings);
+        function fetchReadings() {
+            if (!actions['get-readings']) {
+                debugMessage('no get-readings action defined');
+                return;
+            }
+            jQuery.ajax({
+                url: actions['get-readings'].url,
+                type: actions['get-readings'].method,
+                dataType: "json",
+                success: function (data) {
+                    debugMessage("success", data);
+                    that.readings = [];
+                    data.readings.forEach(function (r) {
+                        that.readings.push(new Reading(r));
+                    });
+                    that.update();
+                }
+            });
+        };
+        that.addMethod("fetchReadings", fetchReadings);
 
-		var createReading = function (attrs) {
-			return new Reading(attrs);
-		};
-		that.addMethod("createReading", createReading,
-			"Creates a new Reading instance");
+        function createReading(attrs) {
+            return new Reading(attrs);
+        };
+        that.addMethod("createReading", createReading,
+            "Creates a new Reading instance");
 
-		//
-		// Private methods & Helpers
-		//
-		var deserializeReading = function (aReading) {
-			if (aReading.when) {
-				aReading.when = new Date(aReading.when);
-			}
-			return aReading;
-		};
+        //
+        // Private methods & Helpers
+        //
+        function deserializeReading(aReading) {
+            if (aReading.when) {
+                aReading.when = new Date(aReading.when);
+            }
+            return aReading;
+        };
 
-		// Register an AJAX error handler to display messages if the
-		// message div exists.
-		var $messageBox = jQuery(sel.message.selector);
-		if ($messageBox) {
-			$messageBox.ajaxError(
-				function (event, jqXHR, ajaxSettings, errorThrown) {
-					var $this = jQuery(this);
-					debugMessage("AJAX Error", jqXHR, errorThrown);
-					$this.find(sel.message.status).text(jqXHR.status);
-					$this.find(sel.message.phrase).text(jqXHR.statusText);
-					$this.find(sel.message.error).text(errorThrown);
-					$this.show();
-				});
-		}
+        // Register an AJAX error handler to display messages if the
+        // message div exists.
+        var $messageBox = jQuery(sel.message.selector);
+        if ($messageBox) {
+            $messageBox.ajaxError(
+                function (event, jqXHR, ajaxSettings, errorThrown) {
+                    var $this = jQuery(this);
+                    debugMessage("AJAX Error", jqXHR, errorThrown);
+                    $this.find(sel.message.status).text(jqXHR.status);
+                    $this.find(sel.message.phrase).text(jqXHR.statusText);
+                    $this.find(sel.message.error).text(errorThrown);
+                    $this.show();
+                });
+        }
 
-		that.actions = actions;
-		that.readings = args.readings || [];
+        that.actions = actions;
+        that.readings = args.readings || [];
 
-		return that;
-	};
+        return that;
+    };
 }(jQuery, window, console));
 
